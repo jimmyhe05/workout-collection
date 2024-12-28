1a9d4da03629e75e05755aed9bfce4c2e78f188f
Jimmy He <jimmyhe05@gmail.com>
2024-12-27 22:12:46 -0700

    fixing route syntax error.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
-import SignOut from './pages/SignUp';
+import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 
@@ -16,8 +16,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signout" element={<SignOut />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>

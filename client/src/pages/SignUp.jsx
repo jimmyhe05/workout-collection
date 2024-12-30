fb94e1a970f9335031be44f58ba0f4fb68d7c9d8
Jimmy He <jimmyhe05@gmail.com>
2024-12-29 23:49:16 -0700

    Finish creating a ui for sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { IoIosFitness } from 'react-icons/io'
+import { Label, TextInput, Button } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left side */}
-        <div>
+        <div className='flex-1'>
           <Link
             to="/"
             className="flex items-center whitespace-nowrap text-md font-bold dark:text-white text-4xl"
@@ -27,7 +28,30 @@ export default function SignUp() {
             Here, I share my personal workout routines along with recommended videos and content from various fitness influencers. Whether you're looking for guidance or inspiration, My Workout Collection helps you stay motivated and track your progress effortlessly.
           </p>
         </div>
-
+        {/* right side */}
+        <div className="flex-1 flex-col gap-3">
+          <form className="flex flex-col gap-2">
+            <div className="">
+              <Label value="EMAIL" />
+              <TextInput type="text" placeholder='' id="email"></TextInput>
+            </div>
+            <div className="">
+              <Label value="USERNAME" />
+              <TextInput type="text" placeholder='' id="username"></TextInput>
+            </div>
+            <div className="mb-2">
+              <Label value="PASSWORD" />
+              <TextInput type="text" placeholder='' id="password"></TextInput>
+            </div>
+            <Button gradientDuoTone="pinkToOrange" type="submit">
+              Join My Workout
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to="/sign-in" className='text-blue-500'>Sign In</Link>
+          </div>
+        </div>
       </div>
     </div >
   )

045fe894271619e671098ad95cd7562e50bc0e55
Jimmy He <jimmyhe05@gmail.com>
2025-01-07 23:35:17 -0700

    adding an error check

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
+if (!process.env.MONGO || !process.env.JWT_SECRET) {
+  throw new Error("Missing required environment variables.");
+}
+
 const app = express();
 app.use(express.json()); // Middleware to parse JSON requests
 
@@ -20,7 +24,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 // Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res
     .status(err.status || 500)
     .json({ message: err.message || "Server Error" });
